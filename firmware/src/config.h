367c3f30644a06fbc8a818b20951a1c33c0ddd01
Clément Dégut <58604417+cdegut@users.noreply.github.com>
2024-11-17 21:57:56 +0000

    minor change

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -12,22 +12,22 @@
 #define XmicroStep 8
 #define XSg_sensitivity_initial 70
 #define XSg_autoeval_divider 2.8  //adjust sensitivity of homing between 2 and 3 
-#define X_MAX_RANGE 65000
+#define X_MAX_RANGE 70000
 #define X_0offset 7500 // Offset from true 0
-#define X_current 1000
+#define X_current 900
 
 #define YdirPin 2
 #define YstepPin 3
 #define YDiagPin 5
 #define YEnPin 4
 #define Y_DRIVER_ADDRESS 0b01 // TMC2209 Driver address according to MS1 and MS2
-#define Yfastspd 200
-#define Yslowspd 400
+#define Yfastspd 250
+#define Yslowspd 450
 #define YmicroStep 8
 #define YSg_sensitivity_initial 70
 #define YSg_autoeval_divider 2.8 //adjust sensitivity of homing between 2 and 3 (3 = less sensitive)
 #define Y_MAX_RANGE 90000
-#define Y_current 1000
+#define Y_current 1100
 
 #define FdirPin D14
 #define FstepPin 15
