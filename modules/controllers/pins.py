55dbda154ea57a719159e9671ac6e777e15cece5
cdegut <c.degut@gmail.com>
2024-11-22 20:59:49 +0000

    clean up

--- modules/controllers/pins.py
+++ modules/controllers/pins.py
@@ -1,33 +1,8 @@
-#Software Endstop (need to be =< to hardware set endstop)
-software_endstops = True
-Xmaxrange = 70000
-Ymaxrange = 93000
-Fmaxrange = 30000
 
-<<<<<<< HEAD:modules/controllers/microscope_param.py
-overshoot_X = -59
-undershoot_X = -23
-overshoot_Y = -62
-undershoot_Y = +61
-=======
-overshoot_X = -16
-undershoot_X = -4
-overshoot_Y = -100
-undershoot_Y = +40
-
-#fluorescent gain value
-awbR_fluo = 1
-awbB_fluo = 0.35
-awbR_white = 3
-awbB_white = 0.8
-
->>>>>>> origin/dev:modules/controllers/pins.py
 #number of retry possible for motor command sent, 10 by default (normal error error rate should not exceed 2 in a row)
 retry = 10
 #number of retry possible for read position, 10 by default (normal error error rate should not exceed 2 in a row)
 read_retry = 10
-
-
 
 # I²C bus address, need to match the arduino address specified in the arduino firmware:  "Wire.begin(addr);"
 addr = 0x8 
